a81816da131199016b07361b8ff282e9d7507abd
Haroon Jalali <haroonjalali@outlook.com>
2024-12-21 10:19:27 -0500

    Delete 1.cpp

--- 1.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>  // Include the input/output stream library
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;  // Print message to the console
-    return 0;  // Indicate that the program ended successfully
-}
